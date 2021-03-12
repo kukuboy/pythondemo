6388b57ff2ef37a0b10e8e4f4e6ef6e18572909a
酷酷男孩 <2294211995@qq.com>
2021-03-12 14:17:33 +0800

    first

---
+++ venv/DEMO.py
@@ -0,0 +1,10 @@
+# -*- codeing = utf-8 -*-
+# @Time : 2021/3/12 11:29
+# @Author : 水印红枫
+# @Software: PyCharm
+
+print("hello,world")
+print("这种标准化输出")
+age=10
+print("这是变量：",age)
+print("这是格式化输出，我的年龄是：%d岁"%age)
