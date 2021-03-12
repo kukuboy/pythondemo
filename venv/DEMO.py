3f8e7a717699f2260eac3fbdb363cf216a829dd3
酷酷男孩 <2294211995@qq.com>
2021-03-12 17:07:04 +0800

    增加输入

--- venv/DEMO.py
+++ venv/DEMO.py
@@ -8,3 +8,13 @@ print("这种标准化输出")
 age=10
 print("这是变量：",age)
 print("这是格式化输出，我的年龄是：%d岁"%age)
+print("我的名字是%s，我的国籍是%s"%("张三","中国"))
+print("www","baidu","com")
+print("www","baidu","com",sep=".")
+print("hello",end="")
+print("world",end="\t")
+print("python",end="\n")
+print("nice to meet you")
+
+password=input("请输入密码：")
+print("你输入的密码是：",password)
