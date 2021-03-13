a758869cc6b78723470a36463e29ade88442fe8a
丁亚辉 <2294211995@qq.com>
2021-03-13 18:27:14 +0800

    增加循环语句

--- venv/DEMO.py
+++ venv/DEMO.py
@@ -3,18 +3,52 @@
 # @Author : 水印红枫
 # @Software: PyCharm
 
-print("hello,world")
-print("这种标准化输出")
-age=10
-print("这是变量：",age)
-print("这是格式化输出，我的年龄是：%d岁"%age)
-print("我的名字是%s，我的国籍是%s"%("张三","中国"))
-print("www","baidu","com")
-print("www","baidu","com",sep=".")
-print("hello",end="")
-print("world",end="\t")
-print("python",end="\n")
-print("nice to meet you")
+# print("hello,world")
+# print("这种标准化输出")
+# age = 10
+# print("这是变量：", age)
+# print("这是格式化输出，我的年龄是：%d岁" % age)
+# print("我的名字是%s，我的国籍是%s" % ("张三", "中国"))
+# print("www", "baidu", "com")
+# print("www", "baidu", "com", sep=".")
+# print("hello", end="")
+# print("world", end="\t")
+# print("python", end="\n")
+# print("nice to meet you")
 
-password=input("请输入密码：")
-print("你输入的密码是：",password)
+# password = input("请输入密码：")
+# print("你输入的密码是：", password)
+
+# a = 10
+# print(type(a))
+#
+# b = input("请输入：")
+# b = int(b)
+# print(type(b))
+
+# if 0 and 1 > 0:
+#     print(true)
+# elif 1:
+#     print("elif")
+# else:
+#     print(false)
+# print("end")
+
+# import random
+#
+# print(random.randint(0, 3))
+
+#
+# for i in range(1, 15, 3):
+#     print(i)
+
+arr = ["aa", "bb"]
+for i in range(len(arr)):
+    print(arr[i])
+
+i = 0
+while i < len(arr):
+    print(arr[i])
+    i += 1
+else:
+    print(i)
