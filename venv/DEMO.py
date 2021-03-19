bd7e6d723c074f4682b1423436b6eed084cb17fc
酷酷男孩 <2294211995@qq.com>
2021-03-19 17:06:03 +0800

    增加元组，链表，字典 增加函数

--- venv/DEMO.py
+++ venv/DEMO.py
@@ -67,5 +67,72 @@
 # print(my_str[::2])
 
 
-print("hello\tworld")
-print(r"hello\tworld")
+# print("hello\tworld")
+# print(r"hello\tworld")
+
+
+# nameList = [1,'aa']
+# print(type(nameList[0]),type(nameList[1])) #数组中可以同时存在不同的类型
+#
+# nameList.append("bb") # 增加数据
+# nameList.extend(['cc','dd',"ee"]) #数据相连
+# nameList.insert(1,"我是被insert进来的") # 在对应位置插入数据
+# del nameList[0] # 删除指定的元素
+# print("我就是被弹出并删除的那个：",nameList.pop()) #弹出并删除最后一个元素
+# print("我就是被移出来的那个：cc",nameList.remove('cc')) #弹出并删除最后一个元素,如果有多个，默认删除第一个
+# print(nameList)
+# print("‘aa’是不是在数组中：",'aa' in nameList)
+# print("‘aa’是不是不在数组中：",'aa' not in nameList)
+# print("‘aa’在数组中指定范围中的位置：", nameList.index('aa',0,len(nameList))) #找不到会报错
+# print("统计‘aa’在数组中出现的次数",nameList.count('aa'))
+#
+# nameList.reverse() #反转
+# nameList.sort() # 排序
+
+'''
+tup1=() # 元组不能更改，可以相加放在新元组中，del可以删除整个元组
+tup2=(20)
+tup3=(20,)
+print(type(tup1),type(tup2),type(tup3))
+'''
+
+# people={"name":"wuyanzu"}
+# # print(people["aaa"]) #会报错
+# print(people.get('aaa',"默认值为none"))
+
+# del(people["name"]) #删除操作 del(people)可删除整个字典
+
+# print(people.clear()) # 清空
+
+# people={"name":"wuyanzu", "age":18}
+# print(people.keys())
+# print(people.values())
+# print(people.items())
+#
+# for key,value in people.items():
+#     print(key,value)
+#
+# myList = ["1","2","3"]
+# print(enumerate(myList))
+# for index,value in enumerate(myList):
+#     print(index,value)
+
+# def math (a,b):
+#     return a//b,a*b
+# a = int(input("请输入你的值："))
+# b = int(input("请输入你的值："))
+# c,d = math(a,b)
+# print("%d和%d的商是：%d,积是：%d"%(a,b,c,d))
+
+a = 100
+def change():
+     a=300
+
+
+def change1():
+    global a
+    a = 400
+change1()
+change()
+print(a)
+
